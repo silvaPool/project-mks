73a16a4643706318a8eb503f3a7523be6bab04e9
silvaPool <silva.friends22@gmail.com>
2024-05-14 16:12:19 -0300

    Component CurrencyFormatter criado

---
+++ src/components/CurrencyFormatter/CurrencyFormatter.tsx
@@ -0,0 +1,16 @@
+import { FunctionComponent } from "react";
+
+import classes from './currency-formatter.module.scss'
+
+interface Props {
+    amount: number
+}
+
+export const CurrencyFormatter: FunctionComponent<Props> = ({ amount }) => {
+    const formattedAmount = amount.toLocaleString('en-GB', {
+      style: 'currency',
+      currency: 'GBP'
+    })
+  
+    return <span className={classes.currency}>{formattedAmount}</span>
+  }
