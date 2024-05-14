4e6435441ae109fdd0b43a2fdfd7f7fe1a84b499
silvaPool <silva.friends22@gmail.com>
2024-05-14 15:03:16 -0300

    Component CartWidget criado

---
+++ src/CartWidget/CartWidget.tsx
@@ -0,0 +1,26 @@
+import { FunctionComponent } from "react";
+
+import  shoppingCart  from "../assets/shopping-cart.svg";
+import classes from './cart-widget.module.scss'
+import { useNavigate } from "react-router-dom";
+
+interface Props {
+    productsCount: number
+}
+
+export const CartWidget: FunctionComponent<Props> = ({productsCount}) => {
+
+    const navigate = useNavigate()
+
+    const navigateToCart = () => {
+        navigate('/cart')
+    }
+
+    return (
+        <button className={classes.container} onClick={navigateToCart}>
+            <span className={classes.productsCount}>{productsCount}</span>
+            <img src={shoppingCart} className={classes.shoppingCart} alt="Go to Cart" />
+            <img src="" alt="" />
+        </button>
+    )
+}
