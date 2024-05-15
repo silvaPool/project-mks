561c2a6fbe806bd56d5b3178925c7e92c583773e
silvaPool <silva.friends22@gmail.com>
2024-05-15 08:51:47 -0300

    Componente Quantifier criado

---
+++ src/components/Quantifier/Quantifier.tsx
@@ -0,0 +1,45 @@
+import { FunctionComponent, useState } from "react"
+
+import classes from './quantifier.module.scss'
+
+export type Operation = 'decrease' | 'increase'
+
+interface Props {
+    removeProductCallback: (productId: number) => void
+    handleUpdateQuantity: (productId: number, operation: Operation) => void
+    productId: number
+}
+
+export const Quantifier: FunctionComponent<Props> = ({removeProductCallback, handleUpdateQuantity, productId}) => {
+    const [value, setValue] = useState<number>(1)
+
+    const reduce = ():void => {
+        handleUpdateQuantity(productId, 'decrease')
+
+        setValue(prevState => {
+            const updateValue = prevState - 1
+            if (updateValue === 0) {
+                removeProductCallback(productId)
+            } 
+            return updateValue
+        })
+    }
+
+    const increase = ():void => {
+        handleUpdateQuantity(productId, 'increase')
+        setValue(prevState => prevState + 1)
+    }
+
+    return (
+        <div className={classes.quantifier}>
+            <input type="button" value="-" className={classes.buttonMinus} onClick={reduce} />
+            <input type="number" 
+                    step="1"
+                    max=""
+                    value={value}
+                    onChange={e => setValue(parseInt(e.target.value))}
+                    className={classes.quantityField}/>
+            <input type="button" value="+" className={classes.buttonPlus} onClick={increase} />
+        </div>
+    )
+}
